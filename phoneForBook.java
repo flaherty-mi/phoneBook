6f169ac995cce0108b7164bb4fc62f3ff90defb6
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-04-27 10:36:42 -0700

    Add files via upload

--- phoneForBook.java
+++ phoneForBook.java
@@ -1,45 +1,53 @@
 // Meghan Flaherty & Adam Curtin
 
-
+// focuses on implimentaing a single linked list
+// opportunity to use package keyword
 
 package phoneBook;
 
 public class phoneForBook {
 
-
-    public phoneForBook(String firstName, String lastName, String address,
-                        String city, String phoneNumber) {
-
-
-    }
     private String firstName;
     private String lastName;
     private String address;
     private String city;
     private String phoneNumber;
 
+
+    // constructor method for phone book and initial contents
+    public phoneForBook(String firstName, String lastName, String address,
+                        String city, String phoneNumber) {
+        setFirstName(String firstName);
+        setLastName(String lastName);
+        setAddress(String address);
+        setCity(String city);
+        setPhoneNumber(String phoneNumber);
+
+    }
+
+    // static method for setting first names
     public static void setFirstName(String firstName) {
-        this.firstName = firstName;
+        firstName = firstName;
     }
 
+    // static method for setting last names
     public static void setLastName(String lastName) {
-        this.lastName = lastName;
+        lastName = lastName;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    // static method for setting addresses
+    public static void setAddress(String address) {
+        address = address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    // static method for setting city values
+    public static void setCity(String city) {
+        city = city;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    // static method for setting phone numbers
+    public static void setPhoneNumber(String phoneNumber) {
+        phoneNumber = phoneNumber;
     }
-
-
-
-
 
 }
