57b44bb193b9f0795b0f48e12c4500795db5f85d
Meghan Flaherty <131192974+flaherty-mi@users.noreply.github.com>
2023-05-06 17:46:41 -0700

    Add files via upload

--- bookManager.java
+++ bookManager.java
@@ -22,15 +22,29 @@ public class bookManager extends bookMain {
 
     }
 
-    public void prompt(LinkedList<Person> Contact) {
-        String choice = null;
-        Scanner input = new Scanner(System.in);
-        menu(Contact);
+    public void rePrompt(LinkedList<Person> Contact) {
+        System.out.println();
+        System.out.println("Would you like to do something else");
+        System.out.println("please type the name of your choice from below");
+        System.out.println("Add: add a new contact");
+        System.out.println("Remove: delete a current contact");
+        System.out.println("Search: find a current contact");
+        System.out.println("Change: modify a current contact's information");
+        System.out.println("Display: display phone book");
+        System.out.println("Quit: quit this program");
 
+       prompt(Contact);
+    }
+
+    public void prompt(LinkedList<Person> Contact) {
+        
+        String choice; 
+        Scanner input = new Scanner(System.in);
         while (input.hasNext()) {
             try {
-                choice = input.next(choice.toLowerCase());
-
+                choice = input.next();
+                choice = choice.toLowerCase();
+                // switch case for 
                 switch (choice) {
                     case "add":
                         addContact(input, Contact);
@@ -72,37 +86,40 @@ public class bookManager extends bookMain {
         System.out.println("Display: display phone book");
         System.out.println("Quit: quit this program");
         System.out.println("Enter the title from the options above");
+        // System.out.println("Enter the title from the options above");
         prompt(Contact);
     }
 
     // method for adding a new contact node
-    public static void addContact(Scanner input, LinkedList<Person> Contact) {
-
+    public void addContact(Scanner input, LinkedList<Person> Contact) {
+        System.out.println();
         System.out.println("Please input the following information as prompted");
-        System.out.print("First name");
+        System.out.print("First name: ");
         firstName = input.next();
         
-        System.out.println("Last name");
+        System.out.print("Last name: ");
         lastName = input.next();
 
-        System.out.println("Address");
+        System.out.print("Address: ");
         address = input.next();
 
-        System.out.println("City");
+        System.out.print("City: ");
         city = input.next();
 
-        System.out.println("Phone number: (360)");
+        System.out.print("Phone number: (360) ");
         phoneNumber = input.next();
         
 
-        if (num.length() != 7) {
-            System.out.println("phone numbers must be 7 digits long." +
-                    "and does not include area code: (360) ");
+        if (phoneNumber.length() != 8) {
+            System.out.println("phone numbers must be 7 digits long" +
+                    "and does not include area code ");
             System.out.println("Please try again: (360) ");
-            num = input.next();
+            phoneNumber = input.next();
         }
         // adding new 'Person' node to the contact
         Contact.add(new Person(firstName, lastName, address, city, phoneNumber));
+        System.out.println("Contact succesfully added");
+        rePrompt(Contact); 
     }
 
     public String toString() {
@@ -142,9 +159,10 @@ public class bookManager extends bookMain {
 
         if (!found) {
             System.out.println("Contact not found.");
+            name = input.next();
         }
 
-
+        rePrompt(Contact);
     }
 
     // method for finding specific contact node
@@ -168,6 +186,7 @@ public class bookManager extends bookMain {
         if (!found) {
             System.out.println("Contact not found.");
         }
+        rePrompt(Contact);
     }
 
     // method for modifying a current contact node
@@ -217,6 +236,7 @@ public class bookManager extends bookMain {
         if (!found) {
             System.out.println("Contact not found.");
         }
+        rePrompt(Contact);
     }
 
     // method for printing entire phone book in ordered fashion
@@ -227,5 +247,6 @@ public class bookManager extends bookMain {
             toString();
             System.out.println(Contact.get(i));
         }
+        rePrompt(Contact);
     } // end displayBook method
 }
